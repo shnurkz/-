6d6e1df839a384fe4cb0975f75b35f7d7e576b6f
Alex <poturayev@gmail.com>
2023-10-30 23:41:37 +0600

    Типы данных и деление

---
+++ Lesson#1/Lesson#1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study
+{
+    internal class Program1
+    {
+        static void Main1(string[] args)
+        {
+            float result;
+            int x, y;
+            x = 5;
+            y = 2;
+            result = Convert.ToSingle(x) / y;
+            Console.WriteLine(result);
+            Console.WriteLine(x);
+            Console.ReadKey();
+        }
+    }
+}
